c57ca9b24fc63221c95d88fcccf69008d18ebcff
Sean Hignett <sean@idstudios.io>
2024-04-05 14:10:16 -0600

    update athena sql

--- eg/iceberg/athena.sql
+++ eg/iceberg/athena.sql
@@ -1,6 +1,6 @@
 --- Athena
 
-CREATE TABLE enriched_orders (
+CREATE TABLE iceberg_enriched_orders_sink_aws (
    order_id INT,
    order_date TIMESTAMP(3),
    customer_name STRING,
@@ -14,5 +14,5 @@ CREATE TABLE enriched_orders (
    destination STRING,
    is_arrived BOOLEAN)
 PARTITIONED BY (order_id)
-LOCATION 's3://ids-flink-demo-warehouse/orders/enriched_orders' -- adjust to your S3 bucket name
+LOCATION 's3://ids-flink-demo-warehouse/iceberg_enriched_orders/default_database/' -- adjust to your S3 bucket name
 TBLPROPERTIES ('table_type'='ICEBERG');
